bbdd9d60b60415bec52321719c4d104f8c7a5e03
dezet98 <danielzaczek124@gmail.com>
2020-10-10 01:36:42 +0200

    create basic internationalization using intl

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,19 @@
+import 'package:engineering_thesis/generated/l10n.dart';
 import 'package:engineering_thesis/shared/components/template_screen.dart';
 import 'package:flutter/material.dart';
-
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TemplateScreen(appBar: AppBar(),);
+    return TemplateScreen(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Center(
+            child: Text(S.of(context).login),
+          ),
+        ],
+      ),
+    );
   }
 }
