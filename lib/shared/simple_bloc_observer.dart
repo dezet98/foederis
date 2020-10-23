f4bb4286d21651534749adb7fc54af8841539367
Daniel Zaczek <50755378+dezet98@users.noreply.github.com>
2020-10-24 01:56:04 +0200

    create basic logger using singleton and logger package

--- lib/shared/simple_bloc_observer.dart
+++ lib/shared/simple_bloc_observer.dart
@@ -1,27 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:engineering_thesis/shared/app_logger.dart';
 
 class SimpleBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object event) {
-    print(event); //TODO refactor this to logger
+    AppLogger().log(message: event.toString(), logLevel: LogLevel.info);
     super.onEvent(bloc, event);
   }
 
   @override
   void onChange(Cubit cubit, Change change) {
-    print(change);
+    AppLogger().log(message: change.toString(), logLevel: LogLevel.info);
     super.onChange(cubit, change);
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
-    print(transition);
+    AppLogger().log(message: transition.toString(), logLevel: LogLevel.info);
     super.onTransition(bloc, transition);
   }
 
   @override
   void onError(Cubit cubit, Object error, StackTrace stackTrace) {
-    print(error);
+    AppLogger().log(message: error.toString(), logLevel: LogLevel.error);
     super.onError(cubit, error, stackTrace);
   }
 }
