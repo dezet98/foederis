8c4e9d5bb4ef976fa931f93d30941caa2864b446
Daniel Zaczek <50755378+dezet98@users.noreply.github.com>
2020-11-20 18:50:01 +0100

    add basic tile for activity

--- lib/screens/home/bottom_nav_bar_content/search_activities/search_activities_screen.dart
+++ lib/screens/home/bottom_nav_bar_content/search_activities/search_activities_screen.dart
@@ -1,4 +1,6 @@
+import 'package:engineering_thesis/models/activity.dart';
 import 'package:engineering_thesis/repositories/activity_repository.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class SearchActivitiesScreen extends StatefulWidget {
@@ -9,25 +11,37 @@ class SearchActivitiesScreen extends StatefulWidget {
 class _SearchActivitiesScreenState extends State<SearchActivitiesScreen> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(54.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Text('Search activity screen'),
-          FutureBuilder(
-            future: ActivityRepository().getAllActivities(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                print(snapshot.error.toString());
-                return Text(snapshot.error.toString());
-              } else if (snapshot.hasData) {
-                return Text(snapshot.data.toString());
-              }
-              return Text('3 option');
-            },
-          )
-        ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Text('Search activity screen'),
+        FutureBuilder(
+          future: ActivityRepository().getAllActivities(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print(snapshot.error.toString());
+              return Text(snapshot.error.toString());
+            } else if (snapshot.hasData) {
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    for (Activity i in snapshot.data) _buildActivityCard(i)
+                  ],
+                ),
+              );
+            }
+            return Text('3 option');
+          },
+        )
+      ],
+    );
+  }
+
+  Widget _buildActivityCard(Activity activity) {
+    return Card(
+      child: ListTile(
+        title: Text(activity.title),
+        subtitle: Text(activity.categoryRef.toString()),
       ),
     );
   }
